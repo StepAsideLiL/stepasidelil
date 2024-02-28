2e2a2ec1ee5c3d16b0b1b306dd01f9af1dd48bdf
Rifat Khan <rifat6633@gmail.com>
2024-02-28 15:53:47 +0600

    add: section start and ending comment

--- app/simple/page.tsx
+++ app/simple/page.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="container min-h-screen py-20 space-y-10">
+      {/* Short Introduction */}
       <section className="text-center space-y-2">
         <h1 className="text-6xl font-medium">Hi 👋, I am Rifat Khan</h1>
         <p className="text-xl text-muted-foreground">
           A JavaScript and Nextjs Developer from Bangladesh
         </p>
       </section>
+      {/* Short Introduction End */}
 
+      {/* Profile Picture and Avatar */}
       <section className="w-32 mx-auto">
         <Image
           src={"/profile-avater.png"}
@@ -19,7 +22,9 @@ export default function Page() {
           className="w-full h-full object-cover"
         />
       </section>
+      {/* Profile Picture and Avatar End */}
 
+      {/* About */}
       <section className="max-w-4xl mx-auto">
         <p className="text-3xl">
           A passionate web developer building cool projects with Nextjs and
@@ -28,6 +33,7 @@ export default function Page() {
           web apps with a sharp eye for UI/UX design.
         </p>
       </section>
+      {/* About End */}
     </main>
   );
 }
