08cfe38719558f106d5aca21937a3e8007b19402
Rifat Khan <rifat6633@gmail.com>
2024-02-28 15:03:44 +0600

    add: introduction section

--- app/simple/page.tsx
+++ app/simple/page.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
-    <main className="min-h-screen py-20">
-      <h1 className="text-center">Hi 👋, I am Rifat Khan</h1>
+    <main className="container min-h-screen py-20 space-y-10">
+      <section className="text-center space-y-2">
+        <h1 className="text-2xl font-medium">Hi 👋, I am Rifat Khan</h1>
+        <p>A JavaScript and Nextjs Developer from Bangladesh</p>
+      </section>
     </main>
   );
 }
