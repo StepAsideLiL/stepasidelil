ce98dfc9fdd14668cd2af96d80d43cc06828704b
Rifat Khan <rifat6633@gmail.com>
2024-02-28 18:18:53 +0600

    add: skills with experties, comfortable and familiar level

--- components/portfolio-infos/infos.tsx
+++ components/portfolio-infos/infos.tsx
@@ -1,2 +1,120 @@
 export const about =
   "A passionate web developer building cool projects with Nextjs and other JavaScript-based libraries. With a background in Engineering, I bring well-rounded problem-solving skills for developing websites and web apps with a sharp eye for UI/UX design.";
+
+export const skills = {
+  topSkills: [],
+  experties: [
+    {
+      title: "HTML",
+      icon: "",
+    },
+    {
+      title: "CSS",
+      icon: "",
+    },
+    {
+      title: "Bootstrap",
+      icon: "",
+    },
+    {
+      title: "TailwindCSS",
+      icon: "",
+    },
+    {
+      title: "JavaScript",
+      icon: "",
+    },
+    {
+      title: "TypeScript",
+      icon: "",
+    },
+    {
+      title: "React",
+      icon: "",
+    },
+    {
+      title: "Nextjs",
+      icon: "",
+    },
+    {
+      title: "Nodejs",
+      icon: "",
+    },
+    {
+      title: "Express",
+      icon: "",
+    },
+    {
+      title: "MongoDB",
+      icon: "",
+    },
+  ],
+  comfortable: [
+    {
+      title: "Redux",
+      icon: "",
+    },
+    {
+      title: "REST api",
+      icon: "",
+    },
+    {
+      title: "JWT",
+      icon: "",
+    },
+    {
+      title: "Stripe",
+      icon: "",
+    },
+    {
+      title: "Git",
+      icon: "",
+    },
+    {
+      title: "Clerk Auth",
+      icon: "",
+    },
+    {
+      title: "Firebase Auth",
+      icon: "",
+    },
+    {
+      title: "Firebase Hosting",
+      icon: "",
+    },
+    {
+      title: "Netlify",
+      icon: "",
+    },
+    {
+      title: "Vercel",
+      icon: "",
+    },
+  ],
+  familiar: [
+    {
+      title: "React Native",
+      icon: "",
+    },
+    {
+      title: "Electron",
+      icon: "",
+    },
+    {
+      title: "Redis",
+      icon: "",
+    },
+    {
+      title: "Docker",
+      icon: "",
+    },
+    {
+      title: "Kubernetes",
+      icon: "",
+    },
+    {
+      title: "AWS",
+      icon: "",
+    },
+  ],
+};
