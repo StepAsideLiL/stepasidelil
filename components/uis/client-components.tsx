4f6e1540ab4057924834d63d4a30abd0110404d8
Rifat Khan <rifat6633@gmail.com>
2024-02-29 18:08:16 +0600

    create: client-component.tsx file and move ModeToggle and CopyEmail component to this file

--- components/uis/client-components.tsx
+++ components/uis/client-components.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import * as React from "react";
+import { cn } from "@/lib/utils";
+import { email } from "@/components/portfolio-infos/infos";
+import { useState } from "react";
+import { Check, Copy, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -10,7 +12,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Moon, Sun } from "lucide-react";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -36,5 +37,29 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+export function CopyEmail({ className }: { className?: string }) {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1500);
+    } catch (error) {
+      console.error("Failed to copy:", error);
+    }
+  };
+
+  return (
+    <div
+      className={cn("flex cursor-pointer items-center gap-2", className)}
+      onClick={() => copyToClipboard()}
+    >
+      <p>{email}</p>
+      <span>{!copied ? <Copy size={16} /> : <Check size={16} />}</span>
+    </div>
   );
 }
