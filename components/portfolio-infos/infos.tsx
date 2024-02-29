b7843abda8ace586d7066debcc6848dbac367317
Rifat Khan <rifat6633@gmail.com>
2024-02-29 15:12:41 +0600

    add: email info

--- components/portfolio-infos/infos.tsx
+++ components/portfolio-infos/infos.tsx
@@ -1,3 +1,5 @@
+export const email = "rifat6633@gmail.com";
+
 export const about =
   "A passionate web developer building cool projects with Nextjs and other JavaScript-based libraries. With a background in Engineering, I bring well-rounded problem-solving skills for developing websites and web apps with a sharp eye for UI/UX design.";
 
