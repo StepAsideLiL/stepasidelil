bcc5547dcdd54f98e812a2f86701dca132e696c4
Rifat Khan <rifat6633@gmail.com>
2024-02-29 17:50:20 +0600

    create: simple page footer component

---
+++ components/uis/simple-page-footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function SimplePageFooter() {
+  return (
+    <footer className="border-t py-10">
+      <p className="text-center">
+        Portfolio of{" "}
+        <Link href={"https://github.com/StepAsideLiL"} className="underline">
+          Rifat Khan
+        </Link>
+      </p>
+    </footer>
+  );
+}
