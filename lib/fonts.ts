96ee13658a79c15e2a3f5e28709f3d66ebd616de
Rifat Khan <rifat6633@gmail.com>
2024-06-21 22:15:53 +0600

    create: fonts.ts and export inter and yesevaOne font

---
+++ lib/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Yeseva_One } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const yesevaOne = Yeseva_One({ subsets: ["latin"], weight: "400" });
