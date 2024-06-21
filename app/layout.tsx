2d250307d611b4d459caae5b8a1e1760902c84b4
Rifat Khan <rifat6633@gmail.com>
2024-06-21 22:15:11 +0600

    remove: font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Rifat Khan (StepAsideLiL)",
