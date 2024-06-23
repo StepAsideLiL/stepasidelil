e12d54b5602c274da131e950e3ed7c0f7139487a
Rifat Khan <rifat6633@gmail.com>
2024-06-23 20:27:10 +0600

    create: velite config file

---
+++ velite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, defineCollection, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+const blogs = defineCollection({
+  name: "Blog",
+  pattern: "blogs/**/*.mdx",
+  schema: s
+    .object({
+      title: s.string(),
+      slug: s.path(), // auto generate slug from file path
+      // slug: s.slug("blogs"),
+      description: s.string().max(999).optional(),
+      date: s.isodate(), // input Date-like string, output ISO Date string.
+      published: s.boolean().default(true),
+      tags: s.array(s.string()).optional(),
+      metadata: s.metadata(), // extract markdown reading-time, word-count, etc.
+      excerpt: s.excerpt(), // excerpt of markdown content
+      content: s.mdx(),
+    })
+    .transform((data) => ({
+      ...data,
+      slugAsParams: data.slug.split("/").slice(1).join("/"),
+      permalink: `/${data.slug}`,
+    })),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: "./.velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { blogs },
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [],
+  },
+});
