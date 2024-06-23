2a3d2ec9b2b0c934e4a63b8284fbb71656e8635b
Rifat Khan <rifat6633@gmail.com>
2024-06-23 23:24:12 +0600

    add: custom a and p component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,10 +1,25 @@
+import { cn } from "@/lib/utils";
 import * as runtime from "react/jsx-runtime";
 
 type MdxProps = {
   content: string;
 };
 
-const components = {};
+const components = {
+  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <a
+      className={cn("underline underline-offset-4", className)}
+      target="_blank"
+      {...props}
+    />
+  ),
+  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+    <p
+      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    />
+  ),
+};
 
 const useMDXComponents = (content: string) => {
   const fn = new Function(content);
