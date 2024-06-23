481645fad26cd712c1a2876d60051f9e9014e95b
Rifat Khan <rifat6633@gmail.com>
2024-06-23 21:13:37 +0600

    update: blog listing style

--- app/(blog)/blogs/page.tsx
+++ app/(blog)/blogs/page.tsx
@@ -1,27 +1,23 @@
 import { yesevaOne } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
-const blogs = [
-  {
-    title: "Is MERN stack still relevant!",
-    href: "/blogs/is-mern-stack-still-relevant",
-  },
-];
+import blogs from "#blogs";
+import { format } from "date-fns";
 
 export default function Page() {
   return (
     <>
       {blogs.map((blog) => (
         <Link
-          key={blog.href}
-          href={blog.href}
-          className={cn(
-            "block py-3 text-4xl text-muted transition-colors hover:text-foreground",
-            yesevaOne.className,
-          )}
+          key={blog.slug}
+          href={blog.permalink}
+          className="block space-y-1 py-3 text-muted transition-colors hover:text-foreground"
         >
-          {blog.title}
+          <h1 className={cn("text-4xl", yesevaOne.className)}>{blog.title}</h1>
+
+          <p className={cn(yesevaOne.className)}>
+            {format(new Date(blog.date), "dd MMMM, yyyy")}
+          </p>
         </Link>
       ))}
     </>
