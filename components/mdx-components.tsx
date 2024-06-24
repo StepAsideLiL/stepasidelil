877c46ae1be24acdf789c0c9e7cf7e95ed8a1aaf
Rifat Khan <rifat6633@gmail.com>
2024-06-24 20:12:38 +0600

    update: add components

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,11 +1,37 @@
 import { cn } from "@/lib/utils";
 import * as runtime from "react/jsx-runtime";
+import {
+  Contact,
+  Education,
+  InterestedIn,
+  Introduction,
+  Projects,
+  TopSkills,
+} from "@/components/portfolio-info-ui";
 
 type MdxProps = {
   content: string;
 };
 
 const components = {
+  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h1
+      className={cn(
+        "font-heading mt-2 scroll-m-20 text-4xl font-bold",
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2
+      className={cn(
+        "font-heading mt-12 scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0",
+        className,
+      )}
+      {...props}
+    />
+  ),
   a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
     <a
       className={cn("underline underline-offset-4", className)}
@@ -19,6 +45,12 @@ const components = {
       {...props}
     />
   ),
+  Introduction,
+  Contact,
+  TopSkills,
+  Projects,
+  Education,
+  InterestedIn,
 };
 
 const useMDXComponents = (content: string) => {
