23f8f9c0a23979f2f6a9af8bcac0342e578034bc
Rifat Khan <rifat6633@gmail.com>
2024-06-24 10:45:24 +0600

    add: Roboto_Mono font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Yeseva_One } from "next/font/google";
+import { Inter, Roboto_Mono, Yeseva_One } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const yesevaOne = Yeseva_One({ subsets: ["latin"], weight: "400" });
+export const robotoMono = Roboto_Mono({ subsets: ["latin"] });
