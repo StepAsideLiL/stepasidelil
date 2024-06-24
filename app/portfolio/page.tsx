dcd070da8896c9e251a18e96e2f95a55713adb6f
Rifat Khan <rifat6633@gmail.com>
2024-06-24 21:36:11 +0600

    update: render portfolio.mdx content

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,24 @@
+import portfilio from "#portfolio";
+import MDXContent from "@/components/mdx-components";
+import { robotoMono } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+
 export default function Page() {
-  return <div>Portfolio Page is loaing...</div>;
+  const myPortfolio = portfilio[0];
+
+  if (!myPortfolio.content) {
+    return (
+      <main
+        className={cn("mx-auto max-w-3xl px-10 py-10", robotoMono.className)}
+      >
+        <h1 className="text-center text-2xl">🥺</h1>
+      </main>
+    );
+  }
+
+  return (
+    <main className={cn("mx-auto max-w-3xl px-10 py-10", robotoMono.className)}>
+      <MDXContent content={myPortfolio.content} />
+    </main>
+  );
 }
