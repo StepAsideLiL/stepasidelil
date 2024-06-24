8db5e6ca4ea3c39ea603b4b8c4f34c37dab4e932
Rifat Khan <rifat6633@gmail.com>
2024-06-24 21:30:08 +0600

    remove: menu components

--- components/uis/client-components.tsx
+++ components/uis/client-components.tsx
@@ -5,41 +5,6 @@ import { useState } from "react";
 import { Check, Copy, Menu, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import Link from "next/link";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-
-const navMenus = [
-  {
-    title: "About",
-    href: "#about",
-  },
-  {
-    title: "Contact",
-    href: "#contact",
-  },
-  {
-    title: "Skills",
-    href: "#skills",
-  },
-  {
-    title: "Projects",
-    href: "#projects",
-  },
-  {
-    title: "Education",
-    href: "#education",
-  },
-  {
-    title: "Interested",
-    href: "#interested",
-  },
-];
 
 export function ModeSwitch() {
   const { theme, systemTheme, setTheme } = useTheme();
@@ -87,52 +52,5 @@ export function CopyEmail({
       <p>{email}</p>
       <span>{!copied ? <Copy size={16} /> : <Check size={16} />}</span>
     </div>
-  );
-}
-
-export function Menus({ className }: { className?: string }) {
-  return (
-    <nav className={cn("flex items-center gap-2", className)}>
-      {navMenus.map((menu) => (
-        <Link key={menu.href} href={menu.href} className="hover:underline">
-          {menu.title}
-        </Link>
-      ))}
-    </nav>
-  );
-}
-
-export function SimpleNavMenuSheet() {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger>
-        <Menu />
-      </SheetTrigger>
-
-      <SheetContent className="flex flex-col gap-2">
-        <div className="h-10"></div>
-
-        <nav className="flex flex-col gap-2">
-          {navMenus.map((menu) => (
-            <Link
-              key={menu.href}
-              href={`${menu.href}`}
-              onClick={() => setOpen(false)}
-              className="hover:underline"
-            >
-              {menu.title}
-            </Link>
-          ))}
-        </nav>
-
-        <Button variant={"secondary"} className="w-fit">
-          <Link href={"/"}>Fancy Portfolio</Link>
-        </Button>
-
-        <ModeSwitch />
-      </SheetContent>
-    </Sheet>
   );
 }
