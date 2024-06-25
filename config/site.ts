96293b570501f5eb20291b47666de314826b5016
Rifat Khan <rifat6633@gmail.com>
2024-06-25 20:55:11 +0600

    add: site info

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "Rifat Khan (StepAsideLiL)",
+  url: "https://stepasidelil.vercel.app/",
+  description: "Personal Portfolio, Projects, and Blogs of Rifat Khan",
+  author: {
+    name: "Rifat Khan",
+    url: "https://stepasidelil.vercel.app/portfolio",
+  },
+  links: {
+    github: "https://github.com/StepAsideLiL",
+    linkedin: "https://www.linkedin.com/in/mdabdullahrifatkhan/",
+    twitter: "https://twitter.com/StepAsideLiL",
+    instagram: "https://www.instagram.com/stepasidelil",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
