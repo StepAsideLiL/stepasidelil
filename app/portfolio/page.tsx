7661822a668d3e5524c26a2840a250e755cdae69
Rifat Khan <rifat6633@gmail.com>
2024-06-25 21:15:27 +0600

    update: add generateMetadata function

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,23 +1,57 @@
 import portfilio from "#portfolio";
 import MDXContent from "@/components/mdx-components";
-import { robotoMono } from "@/lib/fonts";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
-export default function Page() {
-  const myPortfolio = portfilio[0];
+async function getPortfolioData() {
+  return portfilio[0];
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const portfilio = await getPortfolioData();
+
+  if (!portfilio) {
+    return {};
+  }
+
+  return {
+    title: portfilio.title,
+    description: portfilio.description,
+    authors: {
+      name: siteConfig.author.name,
+      url: siteConfig.author.url,
+    },
+    openGraph: {
+      title: portfilio.title,
+      description: portfilio.description,
+      type: "website",
+      url: siteConfig.author.url,
+      images: [
+        {
+          url: `/api/og/portfolio`,
+          width: 1200,
+          height: 630,
+          alt: portfilio.title,
+        },
+      ],
+    },
+  };
+}
+
+export default async function Page() {
+  const myPortfolio = await getPortfolioData();
 
   if (!myPortfolio.content) {
     return (
-      <main
-        className={cn("mx-auto max-w-3xl px-10 py-10", robotoMono.className)}
-      >
+      <main className={cn("mx-auto max-w-3xl px-10 py-10")}>
         <h1 className="text-center text-2xl">🥺</h1>
       </main>
     );
   }
 
   return (
-    <main className={cn("mx-auto max-w-3xl px-10 py-10", robotoMono.className)}>
+    <main className={cn("mx-auto max-w-3xl px-10 py-10")}>
       <MDXContent content={myPortfolio.content} />
     </main>
   );
