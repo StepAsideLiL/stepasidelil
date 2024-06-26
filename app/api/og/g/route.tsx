edc1dec6514f678ec14bc23bdf71d2afc045f8f8
Rifat Khan <rifat6633@gmail.com>
2024-06-26 11:16:31 +0600

    create: api for open graph image

---
+++ app/api/og/g/route.tsx
@@ -0,0 +1,59 @@
+import { ImageResponse } from "next/og";
+import { siteConfig } from "@/config/site";
+import ProfileSvg from "@/components/profile-svg";
+import { NextRequest } from "next/server";
+
+export const runtime = "edge";
+
+const robotoMonoBold = fetch(
+  new URL("../../../../assets/fonts/RobotoMono-Bold.ttf", import.meta.url),
+).then((res) => res.arrayBuffer());
+
+export async function GET(req: NextRequest) {
+  try {
+    const fontBold = await robotoMonoBold;
+
+    const { searchParams } = req.nextUrl;
+    const titleParam = searchParams.get("title");
+
+    const title = titleParam ? titleParam : "Portfolio of Rifat Khan";
+
+    const heading = title.length > 100 ? `${title.slice(0, 100)}...` : title;
+
+    return new ImageResponse(
+      (
+        <div tw="p-10 flex flex-col w-full h-full">
+          <div tw="flex flex-1 flex-col">
+            <div tw="flex items-center">
+              <ProfileSvg size={50} />
+
+              <h1 tw="text-5xl ml-5">{siteConfig.author.name}</h1>
+            </div>
+
+            <p tw="text-7xl">{heading}</p>
+          </div>
+
+          <div tw="flex justify-between">
+            <p>{siteConfig.author.url}</p>
+
+            <p>{siteConfig.links.github}</p>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Roboto Mono",
+            data: fontBold,
+            weight: 700,
+            style: "normal",
+          },
+        ],
+      },
+    );
+  } catch (error) {
+    return new Response("Failed to generate image", { status: 500 });
+  }
+}
