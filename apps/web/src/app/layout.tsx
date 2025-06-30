1f0fdbd8dc99f4e14a51840eed3478cd5b0a693a
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-06-30 15:06:56 +0600

    fix(web): hydration error.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={robotoMono.className} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <body className={robotoMono.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
