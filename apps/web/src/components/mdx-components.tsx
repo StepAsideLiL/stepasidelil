8139b6b7f70056b8396504950c8f3805f25e45c5
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-08-10 11:00:33 +0600

    fix(web): paragraph margin.

--- apps/web/src/components/mdx-components.tsx
+++ apps/web/src/components/mdx-components.tsx
@@ -125,7 +125,7 @@ const components = {
   ),
   p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
     <p
-      className={cn("text-muted-foreground my-2 leading-7", className)}
+      className={cn("text-muted-foreground my-5 leading-6", className)}
       {...props}
     />
   ),
