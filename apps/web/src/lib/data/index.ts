932d11dbd9ef3b70b59353a6da6050e59c5d6f79
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-08-20 16:43:39 +0600

    refactor(web): add limit parameter.

--- apps/web/src/lib/data/index.ts
+++ apps/web/src/lib/data/index.ts
@@ -4,18 +4,20 @@ import projects from "#project";
 const data = {
   blogs,
   projects,
-  getBlogs() {
+  getBlogs(limit?: number) {
     return blogs
       .filter((blog) => blog.published)
-      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+      .slice(0, limit ? limit : blogs.length);
   },
   getBlogBySlug(slug: string) {
     return this.blogs.find((blog) => blog.slugAsParams === slug);
   },
-  getProjects() {
+  getProjects(limit?: number) {
     return projects
       .sort((a, b) => parseInt(b.sort) - parseInt(a.sort))
-      .filter((project) => project.published);
+      .filter((project) => project.published)
+      .slice(0, limit ? limit : projects.length);
   },
   getProjectBySlug(slug: string) {
     return this.projects.find((project) => project.slugAsParams === slug);
